396ad2badabd020d5bdd6d315f6d6c297aab4e44
TunaSandwich <davide.faggionato31@gmail.com>
2024-03-12 11:34:09 +0100

    Refactor: refactored some code

---
+++ src/ts/playlistPage.ts
@@ -0,0 +1,12 @@
+/**
+ * 1) Eseguire una fetch all'endpoint delle playlist
+ * 1.extra) Estrapolare ID da URL
+ *
+ *
+ * 2) convertire risultato della fetch in un array di Track (usando l'apposita interfaccia)
+ *
+ *
+ * 3) renderizzare con una funzione tutti i brani a schermo coi seguenti attributi:
+ * -nome brano
+ * -nome artista (extra: con link)
+ */
